33694ca858d9e185a4342efe28c65c11dad2e5e7
chenxl <tianlangcxl2163.com>
2013-03-24 18:26:36 +0800

    Revert "login_manage"
    This reverts commit 409dd7bde08f9aa9963ec218e98183e42cdccc3d.

--- admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin 
-from cxlsite.books.models import Book, Author, Publisher 
-admin.site.register(Book) 
-admin.site.register(Publisher) 
-admin.site.register(Author)
